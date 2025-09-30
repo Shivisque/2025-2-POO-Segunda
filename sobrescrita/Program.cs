77ea6e6402b8c8dca7640f5c15b830f51d58e940
Shivisque <125708136+Shivisque@users.noreply.github.com>
2025-09-30 00:35:18 +0000

    feat: statement

---
+++ sobrescrita/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
+Console.WriteLine("Hello, World!");
+
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+
+Cartao cartaoBase = new Cartao();
+cartaoBase.Debitar(100);
+
+Console.WriteLine("-- Cartao de Débito --");
+
+CartaoDebito cartaoDebito = new CartaoDebito();
+cartaoDebito.Debitar(100);
